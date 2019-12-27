3576643d815dbacf68eb37c492a24c09009237cd
Alex Korjeski <akorjeski@gmail.com>
2019-12-27 14:14:27 -0500

    almost got the API variable figured out.

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Philadelphia,Pennslvania&units=imperial&appid=" + APIKey;
+var alexander1 = $("#citySearcherinput").val().trim();
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + alexander1 +
+"&units=imperial&appid=" + APIKey;
 var citiesSearched = []; ///this is for the recent searches
 
 
-    // Here we run our AJAX call to the OpenWeatherMap API. 
+    
+  
+      // This function handles events when you add a city to the recently searched array
+      $("#searchButton").on("click", function(event) {
+         
+        event.preventDefault();
+        $("#buttonHolder").empty();
+        // This line grabs the input from the textbox
+        var city = $("#citySearcherinput").val().trim();
+
+        // Adding cties from the textbox to our array
+        citiesSearched.push(city);
+
+        for (var i = 0; i < citiesSearched.length; i++){
+        // Then dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+        // Adding a class of movie-btn to our button
+          a.addClass("city-btn");
+        // Adding a data-attribute
+          a.attr("data-name", citiesSearched[i]);
+        // Providing the initial button text
+          a.text(citiesSearched[i]);
+        // Adding the button to the buttons-view div
+          $("#buttonHolder").append(a);   
+        }
+
+        // Here we run our AJAX call to the OpenWeatherMap API. 
     ///There is another AJAX call nested in here that uses variables from Call 1 to fuel the results for Call 2
     $.ajax({
         url: queryURL,
@@ -37,15 +65,11 @@ var citiesSearched = []; ///this is for the recent searches
 
     });
 
-  
-      // This function handles events when you add a city to the recently searched array
-      $("#searchButton").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var city = $("#citySearcherinput").val().trim();
 
-        // Adding cties from the textbox to our array
-        citiesSearched.push(city);
+
+
+
+        
       });
 
     
