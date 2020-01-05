0a31b32fe3fb0ae1b20f2bccf4587978c6deaccf
Alex Korjeski <akorjeski@gmail.com>
2020-01-05 18:04:43 -0500

    trying out a deeper ajax call

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function fivedayforecast(){
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
   console.log(queryURL);
-  console.log(response);
+  console.log(response.list[4]);
   
   
       })}
