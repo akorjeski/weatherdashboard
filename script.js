477b394d8d1bab5c952a7a0fe42a36fb14417ab2
Alex Korjeski <akorjeski@gmail.com>
2020-01-05 17:33:53 -0500

    testing out the api for 5day forecast with the call

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ $.ajax({
       })
     // We store all of the retrieved data inside of an object called "response"
     .then(function(response) {
-console.log(response.list[4].main.temp);
+console.log(response.list[4]);
 console.log("Alex did this work?");
 
 
